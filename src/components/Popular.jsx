c33e7fda9b0051a21ff25efbdc7b88e5d00961ce
CollyneJumah <collynejumah2010@gmail.com>
2022-05-04 06:17:21 +0300

    updated api key

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 function Popular() {
+    useEffect( ()=>{
+        getRandomRecipe()
+    },[])
+
     const getRandomRecipe = async() => {
-        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}`)
+        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}$number=9`)
+        const data = await api.json()
+        console.log(data)
     }
   return (
     <div>Popular</div>
